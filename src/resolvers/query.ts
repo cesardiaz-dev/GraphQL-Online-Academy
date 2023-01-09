1c62d84158ee70becee8e64bc2eb1e363de4a772
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2023-01-08 23:51:22 -0500

    Consulta de estudiante por ID

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,7 +2,8 @@ import { database } from "../data/data.store.js";
 
 const query = {
     Query: {
-        students: () => database.estudiantes
+        students: () => database.estudiantes,
+        student: (_: void, args: any) => database.estudiantes.filter(estudiante => estudiante.id === args.id)[0]
     }
 };
 
