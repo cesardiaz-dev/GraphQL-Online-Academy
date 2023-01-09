70975d9ac35ba5e7b2c9148528447fae1a0560cd
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2023-01-09 01:05:18 -0500

    Implementacion para agregar curso
    Valida que el nuevo curso no tegna titulo repetido

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,9 +1,45 @@
+import { database } from "../data/data.store.js";
+
 const mutation = {
     Mutation: {
-        // addCourse: (course: any) => {},
+        addCourse: (_: void, args: any): any => {
+            const { course } = args;
+
+            const item = {
+                id: String(database.cursos.length + 1),
+                title: course.title,
+                description: course.description,
+                clases: course.clases,
+                time: course.time,
+                level: course.level,
+                logo: course.logo,
+                path: course.path,
+                teacher: course.teacher,
+                reviews: []
+            };
+
+            // Validar si titulo no existe
+            if(database.cursos.filter(curso => curso.title === item.title).length === 0) {
+                database.cursos.push(item);
+
+                return item;
+            }
+            return {
+                id: "-1",
+                title: `El curso ya existe con este titulo`,
+                clases: -1,
+                time: 0.0,
+                level: "ALL",
+                logo: "",
+                path: "",
+                teacher: "",
+                reviews: []
+            }
+
+        },
         // updateCourse: (course: any) => {},
         // deleteCourse: (id: string) => {},
     }
-};
+}; 
 
 export default mutation;
