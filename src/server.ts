91a0f6edefbaba8343316081acd82dbcdea7fe48
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2023-01-08 20:24:43 -0500

    Configuracion de inicio del servidor

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+import { createServer } from "http";
+
+const app = express();
+app.use("*",cors());
+app.use(compression());
+
+app.get("/", (req, res) => {
+    res.send("Bienvenido a la academia online en GraphQL");
+});
+
+const httpServer = createServer(app);
+
+const PORT = 5200;
+httpServer.listen({ port: PORT },
+    () => console.log(`Servidor listo escuchando en http://localhost:${PORT}`));
